62e7a45cb3e92ace1f3aca16b07afab2b5106e09
LAPTOP <nvminh19051905@gmail.com>
2024-10-11 17:48:21 +0300

    Add more features, add unit tests for helper functions, add credible datasets

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // @ts-nocheck
 
 // lib import
@@ -10,25 +9,12 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 // local import
-=======
-// IMPORT
-require('dotenv').config()
->>>>>>> parent of 30a6a5d4 (fine-tune redirector. debug express-async-error)
 const { MONGOOSE_URL } = require('./util/config')
 const userRouter = require('./controller/user')
 const loginRouter = require('./controller/login')
 const urlRouter = require('./controller/url')
 const redirector = require('./controller/redirector')
-<<<<<<< HEAD
 const phishingDetector = require('./controller/phishing-detector')
-=======
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('express-async-errors')
-
-const mongoose = require('mongoose')
->>>>>>> parent of 30a6a5d4 (fine-tune redirector. debug express-async-error)
 const { errorHandler, requestLogger } = require('./util/middleware')
 
 // mongoose
@@ -39,15 +25,7 @@ mongoose.connect(MONGOOSE_URL)
 app.use(express.json())
 app.use(cors())
 app.use(requestLogger)
-<<<<<<< HEAD
 app.get('/ping', (req, res) => {res.send('pong')})
-=======
-
-app.get('/ping', (req, res) => {
-    res.send('pong')
-})
-
->>>>>>> parent of 30a6a5d4 (fine-tune redirector. debug express-async-error)
 app.use('/api/urls', urlRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
